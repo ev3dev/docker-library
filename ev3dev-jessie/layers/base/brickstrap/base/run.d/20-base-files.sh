dc363c553a48c2157ef9b50a08d2566b1b517696
David Lechner <david@lechnology.com>
2016-10-15 21:14:40 -0500

    ev3dev-jessie: manually copy some files from base-files
    This fixes the ev3dev logo missing from the motd when logging in via ssh.
    
    Since the docker bootstrap is no longer using multistrap, we have to
    install the official debian base-files package first. When upgrading
    the base-files package to the ev3dev version, some of the config files
    are not updated. This manually updates them.

---
+++ ev3dev-jessie/layers/base/brickstrap/base/run.d/20-base-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Since the ev3dev base-files package is installed over the top of the official
+# debian base-files package, some files don't get copied.
+
+set -e
+
+cp /usr/share/base-files/staff-group-for-usr-local  /etc/staff-group-for-usr-local
+cp /usr/share/base-files/nsswitch.conf              /etc/nsswitch.conf
+cp /usr/share/base-files/dot.profile                /root/.profile
+cp /usr/share/base-files/dot.bashrc                 /root/.bashrc
+cp /usr/share/base-files/profile                    /etc/profile
+cp /usr/share/base-files/motd                       /etc/motd
