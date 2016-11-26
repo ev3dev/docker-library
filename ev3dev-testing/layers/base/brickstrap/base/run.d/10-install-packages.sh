3a1044632f378380b5a45859a0ea8f4983fdecce
David Lechner <david@lechnology.com>
2016-11-26 11:18:34 -0600

    ev3dev-testing: Include ev3dev-archive-keyring.gpg instead of downloading
    Apparently apt maintainers don't like using apt-key because it depends
    on gnupg. However, the rest of the world has not caught on yet. Manually
    putting keys in /etc/apt/trusted.gpg.d/ seems to be the "right way" to do
    this.

--- ev3dev-testing/layers/base/brickstrap/base/run.d/10-install-packages.sh
+++ ev3dev-testing/layers/base/brickstrap/base/run.d/10-install-packages.sh
@@ -5,23 +5,11 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-# Have to install gnupg before we can use apt-key
-apt-get update --yes
-apt-get install --yes gnupg
-
-# work around broken dirmngr 2.1.15 package
-apt-get install --yes wget
-wget http://ftp.debian.org/debian/pool/main/g/gnupg2/dirmngr_2.1.16-2_$(dpkg --print-architecture).deb
-dpkg -i dirmngr_2.1.16-2_$(dpkg --print-architecture).deb
-rm dirmngr_2.1.16-2_$(dpkg --print-architecture).deb
-
-# Get the ev3dev archive apt key
-apt-key adv --keyserver pgp.mit.edu --recv-keys D57D95AF93178A7C
-
 # TODO: It would be better to not have the base-files version number hard-coded
 # here. However, it is needed because if debian does a point release, we will
 # actually be downgrading the base-files package.
 
+apt-get update --yes
 apt-get install --yes --allow-downgrades --no-install-recommends \
     alsa-utils \
     avahi-daemon \
