0946e7aa231a4126788afbad6437b3a47571c024
David Lechner <david@lechnology.com>
2017-03-24 16:04:07 -0500

    keyboard setup init scripts are no longer installed by default

--- ev3dev-stretch/layers/base/brickstrap/base/run.d/50-console-setup.sh
+++ ev3dev-stretch/layers/base/brickstrap/base/run.d/50-console-setup.sh
@@ -5,10 +5,3 @@ set -e
 # set the default font
 echo -e -n "\nFONT='Lat15-TomThumb4x6.psf.gz'" >> /etc/default/console-setup
 setupcon --save-only
-
-# keyboard-setup takes long time during boot, so disabling for now.
-update-rc.d keyboard-setup disable
-
-# `kbd` does the same thing as `console-setup`/`keyboard-setup`, so we disable it too,
-# Also, `kbd` breaks systemd, so we really don't want it enabled.
-update-rc.d kbd disable
