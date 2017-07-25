77d28280a32813960953ac892c58d95031f8ac46
David Lechner <david@lechnology.com>
2017-07-25 16:08:16 -0500

    ev3dev-stretch: use our own avahi service files
    The sftp-ssh.conf file has special txt records for use with a vscode extension

--- ev3dev-stretch/layers/base/brickstrap/base/run.d/30-avahi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# make ssh and sftp discoverable
-cp /usr/share/doc/avahi-daemon/examples/ssh.service /etc/avahi/services/
-cp /usr/share/doc/avahi-daemon/examples/sftp-ssh.service /etc/avahi/services/
