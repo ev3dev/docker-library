6c3e34c751136dbd9598de6ec7a92271e57d6cdb
David Lechner <david@lechnology.com>
2017-08-15 13:01:55 -0500

    ev3dev-stretch: enable challange-resonse SSH authentication
    This is needed in order to for the VS Code extension to display
    a password prompt.

--- ev3dev-stretch/layers/base/brickstrap/base/run.d/30-ssh.sh
+++ ev3dev-stretch/layers/base/brickstrap/base/run.d/30-ssh.sh
@@ -4,3 +4,6 @@ set -e
 
 # Allow passing ALL environment variables via ssh
 sed 's/\(AcceptEnv\).*/\1 \*/' -i /etc/ssh/sshd_config
+
+# Allow challenge-response passwords (for VS Code extension)
+sed 's/\(ChallengeResponseAuthentication\).*/\1 yes/' -i /etc/ssh/sshd_config
