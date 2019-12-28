8de0e391360f17aa121cab551b51eea944f2d84a
David Lechner <david@lechnology.com>
2019-12-28 10:51:05 -0600

    ev3dev-stretch: enable bitmap fonts
    Issue: ev3dev/ev3dev#1339

--- ev3dev-stretch/layers/base/brickstrap/base/run.d/31-fontconfig.sh
+++ ev3dev-stretch/layers/base/brickstrap/base/run.d/31-fontconfig.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# We need bitmap fonts for brickman
+rm /etc/fonts/conf.d/70-no-bitmaps.conf
+
 # Build the fontconfig cache. This takes a long time, so do it now rather
 # than the first time an application uses fontconfig. Otherwise, there is a
 # long delay starting brickman on the first boot.
